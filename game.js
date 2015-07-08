84862a1c5031153a47777d82fac89aad18475ea7
Natalie Wolfe <natalie@lifewanted.com>
2015-07-07 17:44:33 -0700

    First bits of menu building.

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
 #! node
 
 var pkg = require('./package');
+var io = require('./lib/interface');
 
 console.log('Welcome to Idle RPG v' + pkg.version);
+
+var main = io.create('main');
+
+main.run()
+    .then(function(){
+        console.log('Thanks for visiting!');
+    }, function(err){
+        console.error('Error:', err);
+        console.error(err.stack);
+    });
