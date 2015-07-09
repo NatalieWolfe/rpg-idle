49eaded819f07f82f9af5c5408baed7ffa6f4973
Natalie Wolfe <natalie@lifewanted.com>
2015-07-08 19:31:49 -0700

    More work on the interface.

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
-#! node
+#! /usr/bin/env node
 
 var pkg = require('./package');
 var io = require('./lib/interface');
 
 console.log('Welcome to Idle RPG v' + pkg.version);
+console.log('');
 
-var main = io.create('main');
+var main = io.create(require('./assets/menus'));
+console.log(main.name);
 
 main.run()
     .then(function(){
